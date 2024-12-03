6379c9cbc469a301ad63f7b158d0d7f6b8f007c2
apple <apple@apples-MacBook-Pro.local>
2024-12-03 22:50:17 +0530

    problem 1 completed

--- basic-fundamental-problems/Problem001.java
+++ basic-fundamental-problems/Problem001.java
@@ -1,5 +1,8 @@
+// Print Numbers from 1 to 10
 public class Problem001 {
     public static void main(String[] args) {
-        
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(i + " ");
+        }
     }
 }
